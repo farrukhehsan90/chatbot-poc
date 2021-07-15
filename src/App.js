0b98b2bbba070b8eb55e107166cd7e963f059162
Farrukh <farrukhehsan90@gmail.com>
2021-07-16 03:46:19 +0500

    initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    
+    <iframe width="350" height="430" allow="microphone;" src="https://console.dialogflow.com/api-client/demo/embedded/519c99f5-91ca-4e37-977d-78aafec7031e"></iframe>
+    </div>
+  );
+}
+
+export default App;
